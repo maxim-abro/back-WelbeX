5763ee14e411e282595443c8ae4229d31843456c
Абросимов Максим <abrosimov@NCM.lo>
2022-10-11 17:48:26 +0300

    correct get request

--- table/table.service.js
+++ table/table.service.js
@@ -3,26 +3,36 @@ const axios = require("axios");
 const { Op, fn, col } = require("sequelize");
 
 module.exports = {
+  // async getAll(req, res) {
+  //   try {
+  //     const limit = 30;
+  //     const count = await db.Table.count();
+  //     const elements = await db.Table.findAndCountAll({
+  //       offset: calcPage(limit, req.query.page ? req.query.page : 1),
+  //       order: [
+  //           [req.query.col || "id", req.query.order || "ASC"]
+  //         ],
+  //       limit,
+  //     });
+  //
+  //     return res.status(200).send({
+  //       pagination: {
+  //         total_pages: Math.ceil(count / limit),
+  //         total_elements: count,
+  //         current_page: req.query.page * 1 || 1
+  //       },
+  //       rows: elements.rows
+  //     });
+  //   } catch (e) {
+  //     console.log(e);
+  //     return res.status(500).send(e);
+  //   }
+  // },
   async getAll(req, res) {
     try {
-      const limit = 30;
-      const count = await db.Table.count();
-      const elements = await db.Table.findAndCountAll({
-        offset: calcPage(limit, req.query.page ? req.query.page : 1),
-        order: [
-            [req.query.col || "id", req.query.order || "ASC"]
-          ],
-        limit,
-      });
+      const elements = await db.Table.findAll();
 
-      return res.status(200).send({
-        pagination: {
-          total_pages: Math.ceil(count / limit),
-          total_elements: count,
-          current_page: req.query.page * 1 || 1
-        },
-        rows: elements.rows
-      });
+      return res.status(200).send(elements);
     } catch (e) {
       console.log(e);
       return res.status(500).send(e);
@@ -41,7 +51,7 @@ module.exports = {
   },
   // async parsingTables(req, res) {
   //   try {
-  //     const words = await axios.get("http://localhost:4000/word/mnogo");
+  //     const words = await axios.get("http://localhost:4000/word/mnogo/");
   //
   //     for (let i of words.data) {
   //       const toDb = new db.Table({
